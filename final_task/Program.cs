952cec0ab93ec6b21659d99e643379ae53a0117b
Mary Nudnova <m.nudnova@yandex.ru>
2024-02-27 22:29:42 +0300

    make an array from the string

--- final_task/Program.cs
+++ final_task/Program.cs
@@ -1,2 +1,4 @@
 ﻿Console.Write("Fill in the elements of the future array: ");
 string str = Console.ReadLine();
+str = str.Replace(" ", ""); // убрали промежутки
+string[] array = str.Split(","); // сделали массив из строки
