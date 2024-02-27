06af2678859035fd14cb28d05bfda26cc4fda6f1
Mary Nudnova <m.nudnova@yandex.ru>
2024-02-27 22:30:24 +0300

    create method to print an array

--- final_task/Program.cs
+++ final_task/Program.cs
@@ -2,3 +2,12 @@
 string str = Console.ReadLine();
 str = str.Replace(" ", ""); // убрали промежутки
 string[] array = str.Split(","); // сделали массив из строки
+
+void PrintArray(string[] array) 
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
